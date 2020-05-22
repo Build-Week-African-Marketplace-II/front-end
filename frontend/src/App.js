5ab51a024235320fcc854bf75611fccf6d0ab488
Luis Pineda <lspnd00@gmail.com>
2020-05-21 22:05:28 -0400

    added 3 components login logout dashboard, routes as well

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import LogIn from './components/LogIn'
+import SignUp from './components/SignUp'
+
+import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/LogIn">Log In</Link>
+            <Link to="/SignUp">Sign Up</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/LogIn">
+            <LogIn />
+          </Route>
+          <Route exact path="/SignUp">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
