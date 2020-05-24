fda6390a47df667979aecce5abf7db660f530f79
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2020-05-24 18:56:26 -0500

    add basic state for signup form

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+
+  }
+
   return (
     <div>
       <h1>Sign Up Form</h1>
+      <form onSubmit={submitHandler}>
+
+      </form>
     </div>
   )
 }
